29fa227abf865809653fda701f2c2b23bdefbb1a
Anonymous <anonymous@lukevanin.staff.hq.takealot.com>
2017-03-07 17:05:27 +0200

    Added photo library. Updated colors.

---
+++ CardScanner/PhotoLibraryCell.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryCollectionViewCell.swift
+//  CardScanner
+//
+//  Created by Luke Van In on 2017/03/05.
+//  Copyright © 2017 Luke Van In. All rights reserved.
+//
+
+import UIKit
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ CardScanner/PhotoLibraryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoLibraryViewController.swift
+//  CardScanner
+//
+//  Created by Luke Van In on 2017/03/05.
+//  Copyright © 2017 Luke Van In. All rights reserved.
+//
+
+import Foundation
